a5aba37b3f8b983e0479cf0d2334428b6a593376
James <info@thecodediver.com>
2020-11-13 12:16:09 -0500

    complete api functionality with all CRUD operations

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth"
 
 const initialColor = {
   color: "",
@@ -18,13 +18,26 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors(colors.map(item => {
+          if(item.id === res.data.id) {
+            return res.data
+          }
+          return item
+        }))
+      })
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        updateColors(colors.filter(item => {
+          return item.id !== color.id
+        }))
+      })
   };
 
   return (
